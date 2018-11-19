5f236dcdb90b0459495d6769b8337de567c39b2b
187680 <187680@edu.p.lodz.pl>
2018-11-19 21:43:13 +0100

    setting up bridge branch

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,8 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    private defaultCountingOutRhymer dcor = new defaultCountingOutRhymer();
+    private DefaultCountingOutRhymer dcor = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
